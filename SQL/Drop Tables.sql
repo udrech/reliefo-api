da14e6f8bcae579807c0276c3ee0ebd85307460f
Urs Dreher <ursdreher@hotmail.com>
2026-04-03 00:37:56 +0200

    Testdaten und Löschscript erstellt

---
+++ SQL/Drop Tables.sql
@@ -0,0 +1,6 @@
+DROP TABLE "__EFMigrationsHistory";
+DROP TABLE receipts;
+DROP TABLE medical_histories;
+DROP TABLE appointments;
+DROP TABLE therapies;
+DROP TABLE customers;
---
+++ SQL/Testdata.sql
@@ -0,0 +1,28 @@
+-- PostgreSQL Testdata
+-- TRUNCATE TABLE customers;
+INSERT INTO customers (lastname, firstname, address, city, zipcode, mobile, created_at, updated_at)
+VALUES ('Dreher', 'Urs', 'Warteggstrasse 10', 'Rorschacherberg', '9404', '079 295 83 06', NOW(), NOW());
+INSERT INTO customers (lastname, firstname, address, city, zipcode, mobile, created_at, updated_at)
+VALUES ('Oettli', 'Sabina', 'Warteggstrasse 10', 'Rorschacherberg', '9404', '079 285 60 11', NOW(), NOW());
+
+-- TRUNCATE TABLE therapies;
+INSERT INTO therapies (name, description, duration, price, valid_until, created_at, updated_at)
+VALUES ('Klassische Massage', 'Massage nach Wunsch - ganzer Körper, Rücken, etc.', 60, 90, '2026-12-31 23:00:00', NOW(), NOW());
+
+-- TRUNCATE TABLE appointments;
+INSERT INTO appointments (customers_id, therapies_id, appointment_timestamp, created_at, updated_at)
+VALUES (1, 1, '2026-04-10 10:00:00', NOW(), NOW());
+INSERT INTO appointments (customers_id, therapies_id, appointment_timestamp, created_at, updated_at)
+VALUES (2, 1, '2026-04-11 14:00:00', NOW(), NOW());
+
+-- TRUNCATE TABLE medical_histories;
+INSERT INTO medical_histories (customers_id, history_timestamp, type, note, created_at, updated_at)
+VALUES (1, '2026-04-10 10:00:00', 'Medikamente', 'Blutdrucksenkung', NOW(), NOW());
+INSERT INTO medical_histories (customers_id, history_timestamp, type, note, created_at, updated_at)
+VALUES (2, '2026-04-11 14:00:00', 'Beschwerde', 'Verspannungen im Schulterbereich, vor allem links', NOW(), NOW());
+
+-- TRUNCATE TABLE receipts;
+INSERT INTO receipts (customers_id, receipt_timestamp, file, data, created_at, updated_at)
+VALUES (1, '2026-04-10 10:00:00', 'quittung.pdf', '{}', NOW(), NOW());
+INSERT INTO receipts (customers_id, receipt_timestamp, file, data, created_at, updated_at)
+VALUES (2, '2026-04-11 14:00:00', 'quittung.pdf', '{}', NOW(), NOW());
